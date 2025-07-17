5b0548257df0ae23aac3644d8dbcc84e3785eebd
ayrh90-fZNzxW <ayrh90@icloud.com>
2025-07-16 20:37:59 -0700

    function 1 done

--- main.js
+++ main.js
@@ -13,10 +13,53 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+const pAequorFactory = (specimenNum, dna) => {
+  
+  let pAequor = {
+    
+    _specimenNum: specimenNum,
+    _dna: dna,
+    mutate() {
+      let baseIndex = Math.floor(Math.random() * this._dna.length);
+      let replacementBase = ''; 
 
+      console.log(`Original base ${this._dna[baseIndex]}`);
+      do{
+        replacementBase = returnRandBase();
+      }while(replacementBase === this._dna[baseIndex]);
+      console.log(replacementBase); 
 
+     this._dna[baseIndex] = replacementBase; 
+     console.log(`Replaced base ${this._dna[baseIndex]}`); 
+     return this._dna; 
+    },
 
+  };
+  
+  return pAequor; 
+}
 
+const monsterOne = pAequorFactory(125, mockUpStrand()); 
+console.log(monsterOne);
+monsterOne.mutate(); 
+console.log(monsterOne)
 
+/*
 
+better versions of the functions 
 
+mutate() {
+  const bases = ['A', 'T', 'C', 'G'];
+  const index = Math.floor(Math.random() * this._dna.length);
+  const current = this._dna[index];
+
+  // Remove current base and pick a random one from the remaining
+  const newBase = bases.filter(base => base !== current)[
+    Math.floor(Math.random() * 3)
+  ];
+
+  this._dna[index] = newBase;
+  return this._dna;
+}
+
+*/
